b302df7c8e5350ef2d7e5ee45775294844bb45fb
Michel Alves <michelalvs@gmail.com>
2021-05-13 20:34:52 -0300

    Finished casting Lecture

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int a, b;
+
+        double resultado;
+
+        a = 5;
+        b = 2;
+
+        resultado = (double) a / b; // Para efetuar Casting, coloque o tipo de variável de saída desejada antes do resultado e entre parênteses.
+
+        System.out.print(resultado);
+    }
+}
